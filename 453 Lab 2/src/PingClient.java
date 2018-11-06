1fe5bd049bcdb31ec25c4f5c37e65c36bbb21048
lemowery <levimowery@outlook.com>
2018-11-06 14:10:36 -0500

    Finished template, havent tested.

--- 453 Lab 2/src/PingClient.java
+++ 453 Lab 2/src/PingClient.java
@@ -4,18 +4,17 @@ import java.util.*;
 
 public class PingClient {   
   
-   
-  public static void main(String[] args)throws Exception {   
+public static void main(String[] args)throws Exception {   
     
-    long totalrtt = 0;
+	long totalrtt = 0;
     long maxrtt = -9999;
     long minrtt = 9999;
     int drops = 0;
     int retPacket;
     
     if (args.length != 2) {  // check if number of arguments are correct
-      System.out.println("Required arguments: host port");   
-      return;   
+    	System.out.println("Required arguments: host port");   
+    	return;   
     }   
     String server = args[0];   // Read first argument from user 
     String serport = args[1]; // Read second argument from user
@@ -29,45 +28,57 @@ public class PingClient {
     byte[] receiveData = new byte[1024];   
     
     for(int i = 0; i < 10; i++) {  
-      Long time = new Long(System.currentTimeMillis());
-      //String payload = ?; // Construct data payload for PING as per the instructions
-      //sendData = ?; // Convert payload into bytes
-      //DatagramPacket packet = new DatagramPacket(?);    // Create new datagram packet
-      //?; // send packet
-      
-      //DatagramPacket reply = ?; // Create datagram packet for reply
-      
-      try {
-        //?; wait for incoming packet reply
-        byte[] buf = reply.getData();          
-        ByteArrayInputStream bais = new ByteArrayInputStream(buf);   
-        InputStreamReader isr = new InputStreamReader(bais);   
-        BufferedReader br = new BufferedReader(isr);
-        String line = br.readLine();   
+    	Long time = new Long(System.currentTimeMillis());
+    	String payload = "PING " + i + " " + time + " " + "\r\n"; // Construct data payload for PING as per the instructions
+    	sendData = payload.getBytes(); // Convert payload into bytes
+    	DatagramPacket packet = new DatagramPacket(sendData, sendData.length);    // Create new datagram packet
+    	socket.send(packet); // send packet
+    	DatagramPacket reply = new DatagramPacket(new byte[1024], 1024); // Create datagram packet for reply
+    	
+    	try {
+    		socket.receive(reply);; //wait for incoming packet reply
+    		byte[] buf = reply.getData();          
+    		ByteArrayInputStream bais = new ByteArrayInputStream(buf);   
+    		InputStreamReader isr = new InputStreamReader(bais);   
+    		BufferedReader br = new BufferedReader(isr);
+    		String line = br.readLine();   
         
-	  // Parse incoming string "line"
-
-	  // extract packet sequence number into the variable retPacket
-        //retPacket = ?
+    		// Parse incoming string "line"
+    		String[] data = line.split(" ");
         
-        if (retPacket != i) {   
-          System.out.print("Received out of order packet");
-          System.out.println();
-        }
-	  else {
-	    System.out.println("Received from " + request.getAddress().getHostAddress() + " ," + new String(line)); 
-          System.out.println();
-          //long rtt = ?;  // calculate roundtrip time
-          // calculate total, max and min rtt
-        }        
-      } 
-      catch(SocketTimeoutException e) {
-        System.out.println("Error: Request timed out");
-        drops++;
-      }   
+    		// extract packet sequence number into the variable retPacket
+    		retPacket = Integer.parseInt(data[1]);
+        
+    		if (retPacket != i) {   
+    			System.out.print("Received out of order packet");
+    			System.out.println();
+    		}
+    		else {
+    			System.out.println("Received from " + reply.getAddress().getHostAddress() + " ," + new String(line)); 
+    			System.out.println();
+    			long rtt = System.currentTimeMillis() - Integer.parseInt(data[2]);  // calculate roundtrip time
+          
+    			// calculate total, max and min rtt
+    			totalrtt += rtt;
+    			if(rtt < minrtt) {
+    				minrtt = rtt;
+    			}
+    			if(rtt > rtt) {
+    				maxrtt = rtt;
+    			}
+    		}        
+    	} 
+    	catch(SocketTimeoutException e) {
+    		System.out.println("Error: Request timed out");
+    		drops++;
+    	}   
     } 
-    //long avgrtt = ?; calculate average rtt
+    long avgrtt = totalrtt / 10; //calculate average rtt
     
     // print and store average, max, min rtt and drops
+    System.out.printf("Average RTT: %d\n"
+    		+ "Max RTT: %d\n"
+    		+ "Min RTT: %d\n"
+    		+ "Drops: %d\n", avgrtt, maxrtt, minrtt, drops);
   }    
 }   
